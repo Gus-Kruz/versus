aa1d530bded6962dc20664d2183263c4027860ea
Gus_Kruz <85446865+Gus-Kruz@users.noreply.github.com>
2025-06-15 21:32:55 -0300

    Update script.js

--- script.js
+++ script.js
@@ -3,6 +3,14 @@ let souJogador1 = false;
 let minhaPalavra = "";
 let palavraDoAdversario = "";
 
+function gerarCodigoSala() {
+  const caracteres = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+  let codigo = "";
+  for (let i = 0; i < 5; i++) {
+    codigo += caracteres.charAt(Math.floor(Math.random() * caracteres.length));
+  }
+  return codigo;
+}
 // Cria uma nova sala
 function criarSala() {
   const codigo = gerarCodigoSala();
